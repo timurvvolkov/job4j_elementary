53a17d37847790606e5583efdba896a04a7e991d
timurvvolkov <timurvvolkov@gmail.com>
2020-03-18 16:59:19 +0300

    3.3. Разбор первой программы.[#256732]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Timur Volkov");
+        System.out.println("07.06.1977");
+    }
+}
