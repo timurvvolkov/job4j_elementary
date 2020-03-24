d38eb61e961759aace75b9a9e08085dc2e686738
timurvvolkov <timurvvolkov@gmail.com>
2020-03-24 16:13:54 +0300

    4.5. Отладка программы в IDEA[#256676]

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -1,17 +1,10 @@
 package ru.job4j.condition;
 
 public class SqArea {
-    public static double square(int p, int k) {
-        double h = p / (2 * (k + 1));
-        double l = h * k;
-        double rsl = l * h;
-        return rsl;
-    }
-
-    public static void main(String[] args) {
-        double result1 = SqArea.square(4, 1);
-        System.out.println(" p = 4, k = 1, s = 1, real = " + result1);
-        double result2 = SqArea.square(6, 2);
-        System.out.println(" p = 6, k = 2, s = 2, real = " + result2);
+    public static int square(int p, int k) {
+        int h = p / (2 * (k + 1));
+        int w = k * h;
+        int s = w * h;
+        return s;
     }
 }
--- test/ru/job4j/condition/SqAreaTest.java
+++ test/ru/job4j/condition/SqAreaTest.java
@@ -1,15 +1,27 @@
 package ru.job4j.condition;
 
-import org.junit.Assert;
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
 public class SqAreaTest {
+
     @Test
-    public void square() {
-        int in1 = 6;
-        int in2 = 2;
-        double expected = 2.0;
-        double out = SqArea.square(in1, in2);
-        Assert.assertEquals(expected, out, 0.01);
+    public void whenp4k1s1() {
+        int k = 1;
+        int p = 4;
+        int expected = 1;
+        int s = SqArea.square(p, k);
+        assertThat(s, is(expected));
+    }
+
+    @Test
+    public void whenp6k2s2() {
+        int k = 2;
+        int p = 6;
+        int expected = 2;
+        int s = SqArea.square(p, k);
+        assertThat(s, is(expected));
     }
 }
