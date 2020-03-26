0c48a1d9d3f6b92badfa4393ffccb2a25bd0ff5f
timurvvolkov <timurvvolkov@gmail.com>
2020-03-26 15:50:22 +0300

    6.0.2. Заполнение массива.[#256701]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,15 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+
+        String[] names = new String[4];
+        names[0] = "Ivan Ivanov";
+        names[1] = "Alexey Kovalev";
+        names[2] = "Sergey Kuznetsov";
+        names[3] = "Maxim Sidorov";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
