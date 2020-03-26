8a4bb098af0fe23f21dbba2d77a564a7cc2bc9a2
timurvvolkov <timurvvolkov@gmail.com>
2020-03-26 09:59:29 +0300

    5.8. JavaDoc. Code convention[#256763]

--- src/ru/job4j/calculator/Calculator.java
+++ src/ru/job4j/calculator/Calculator.java
@@ -1,10 +1,21 @@
 package ru.job4j.calculator;
 
+/**
+ * Calculator
+ *
+ * Класс для вычисления арифметических операций
+ * @author Timur Volkov (timurvvolkov@gmail.com)
+ */
 public class Calculator {
     public static void plus(int first, int second) {
         int result = first + second;
         System.out.println(result);
     }
+
+    /**
+     * Method main.
+     * @param args - arguments
+     */
     public static void main(String[] args) {
         Calculator.plus(1, 2);
         Calculator.plus(10, 11);
---
+++ src/ru/job4j/calculator/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Timur Volkov (timurvvolkov@gmail.com)
+ * @version 1
+ * @since 26.03.2020
+ */
+package ru.job4j.calculator;
