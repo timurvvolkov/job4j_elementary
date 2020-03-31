0679ad8af63329d0cb0d34b4077c1db4151d803d
timurvvolkov <timurvvolkov@gmail.com>
2020-03-31 11:17:37 +0300

    1. Экзамен. Базовый синтаксис.[#256748]

---
+++ src/ru/job4j/sort/Merge.java
@@ -0,0 +1,46 @@
+package ru.job4j.sort;
+
+import java.util.Arrays;
+
+public class Merge {
+    public int[] merge(int[] left, int[] right) {
+        int[] rsl = new int[left.length + right.length];
+        int leftIndex = 0;
+        int rightIndex = 0;
+        int rslIndex = 0;
+        while (rslIndex < rsl.length) {
+            if (leftIndex == left.length) {
+                rsl[rslIndex] = right[rightIndex];
+                rightIndex++;
+                rslIndex++;
+                continue;
+            }
+
+            if (rightIndex == right.length) {
+                rsl[rslIndex] = left[leftIndex];
+                leftIndex++;
+                rslIndex++;
+                continue;
+            }
+
+            if (left[leftIndex] < right[rightIndex]) {
+                rsl[rslIndex] = left[leftIndex];
+                leftIndex++;
+            } else {
+                rsl[rslIndex] = right[rightIndex];
+                rightIndex++;
+            }
+            rslIndex++;
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        Merge process = new Merge();
+        int[] rsl = process.merge(
+                new int[] {1, 3, 5},
+                new int[] {2, 4}
+        );
+        System.out.println(Arrays.toString(rsl));
+    }
+}
