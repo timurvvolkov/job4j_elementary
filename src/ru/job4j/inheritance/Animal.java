dd87e90a3f0b82011c42eb20342ab6dc39e442dd
timurvvolkov <timurvvolkov@gmail.com>
2020-04-07 12:24:03 +0300

    1. Конструктор по умолчанию.[#256852]

---
+++ src/ru/job4j/inheritance/Animal.java
@@ -0,0 +1,8 @@
+package ru.job4j.inheritance;
+
+public class Animal {
+    public Animal() {
+        super();
+        System.out.println("Animal");
+    }
+}
---
+++ src/ru/job4j/inheritance/Predator.java
@@ -0,0 +1,8 @@
+package ru.job4j.inheritance;
+
+public class Predator extends Animal {
+    public Predator() {
+        super();
+        System.out.println("Predator");
+    }
+}
