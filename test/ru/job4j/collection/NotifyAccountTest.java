95707ac83994417a63ff2213e7633aad45720052
timurvvolkov <timurvvolkov@gmail.com>
2020-05-01 17:30:52 +0300

    3. Модели данных и HashSet.[#256801]

---
+++ test/ru/job4j/collection/NotifyAccountTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class NotifyAccountTest {
+    @Test
+    public void sent() {
+        List<Account> accounts = Arrays.asList(
+                new Account("123", "Petr Arsentev", "eDer3432f"),
+                new Account("142", "Petr Arsentev", "000001")
+        );
+        HashSet<Account> expect = new HashSet<>(
+                Arrays.asList(
+                        new Account("123", "Petr Arsentev", "eDer3432f"),
+                        new Account("142", "Petr Arsentev", "000001")
+                )
+        );
+        assertThat(NotifyAccount.sent(accounts), is(expect));
+    }
+
+    @Test
+    public void sentDub() {
+        List<Account> accounts = Arrays.asList(
+                new Account("757", "Ivan Ivanov", "ABCDEF"),
+                new Account("757", "Timur Volkov", "000101")
+        );
+        HashSet<Account> expect = new HashSet<>(
+                Arrays.asList(
+                        new Account("757", "Timur Volkov", "000101")
+                )
+        );
+        assertThat(NotifyAccount.sent(accounts), is(expect));
+    }
+}
