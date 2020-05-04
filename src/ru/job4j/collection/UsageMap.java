cef41e3e30283dbdb07931433a64901f9de9a3cc
timurvvolkov <timurvvolkov@gmail.com>
2020-05-04 15:41:09 +0300

    0. Map, HashMap[#256794]

---
+++ src/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("timur_volkov@yandex.ru", "Timur Volkov");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
