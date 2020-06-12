90f01243378391047657725cd10459738030b2b8
timurvvolkov <timurvvolkov@gmail.com>
2020-06-12 12:31:53 +0300

    2. Список адресов [#256658]

---
+++ test/ru/job4j/lambda/ProfilesTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.lambda;
+
+import org.junit.Test;
+import ru.job4j.streamapi.Address;
+import ru.job4j.streamapi.Profile;
+import ru.job4j.streamapi.Profiles;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ProfilesTest {
+    @Test
+    public void test1() {
+        Address address1 = new Address("Boston", "57th", 22, 134);
+        Address address2 = new Address("Seattle", "Ocean ave", 7, 3);
+        Profile profile1 = new Profile(address1);
+        Profile profile2 = new Profile(address2);
+        List<Profile> profiles = Arrays.asList(profile1, profile2);
+        List<Address> expect = Arrays.asList(address1, address2);
+        List<Address> result = Profiles.collect(profiles);
+        assertThat(result, is(expect));
+    }
+}
