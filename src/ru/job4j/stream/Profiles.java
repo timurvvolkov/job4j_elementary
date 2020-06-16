ec263effbe8fb7e132492d9a4731f79b69b70ed6
timurvvolkov <timurvvolkov@gmail.com>
2020-06-16 11:31:53 +0300

    3. Уникальность элементов и сортировка. [#256659]

--- src/ru/job4j/stream/Profiles.java
+++ src/ru/job4j/stream/Profiles.java
@@ -5,6 +5,10 @@ import java.util.stream.Collectors;
 
 public class Profiles {
     public static List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(profile -> profile.getAddress()).collect(Collectors.toList());
+        return profiles.stream()
+                       .map(profile -> profile.getAddress())
+                       .sorted(new AddressRiseByName())
+                       .distinct()
+                       .collect(Collectors.toList());
     }
 }
--- test/ru/job4j/stream/ProfilesTest.java
+++ test/ru/job4j/stream/ProfilesTest.java
@@ -20,4 +20,20 @@ public class ProfilesTest {
         List<Address> result = Profiles.collect(profiles);
         assertThat(result, is(expect));
     }
+
+    @Test
+    public void test2() {
+        Address address1 = new Address("Seattle", "Ocean ave", 4, 5);
+        Address address2 = new Address("Boston", "57th", 22, 134);
+        Address address3 = new Address("Seattle", "Ocean ave", 7, 3);
+        Address address4 = new Address("Boston", "57th", 22, 134);
+        Profile profile1 = new Profile(address1);
+        Profile profile2 = new Profile(address2);
+        Profile profile3 = new Profile(address3);
+        Profile profile4 = new Profile(address4);
+        List<Profile> profiles = Arrays.asList(profile1, profile2, profile3, profile4);
+        List<Address> expect = Arrays.asList(address2, address1, address3);
+        List<Address> result = Profiles.collect(profiles);
+        assertThat(result, is(expect));
+    }
 }
