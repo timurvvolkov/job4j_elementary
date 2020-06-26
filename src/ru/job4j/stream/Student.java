1ab9c4bdcecac486c67c1b2014716bc07e36e2be
timurvvolkov <timurvvolkov@gmail.com>
2020-06-26 12:15:58 +0300

    0. Stream API улучшения [#256664]

--- src/ru/job4j/stream/Student.java
+++ src/ru/job4j/stream/Student.java
@@ -1,27 +1,39 @@
 package ru.job4j.stream;
 
+import java.util.Objects;
+
 public class Student {
-    private String surname;
     private int score;
+    private String surname;
 
-    public Student(String surname, int score) {
+    public void setSurname(String surname) {
         this.surname = surname;
-        this.score = score;
-    }
-
-    public String getSurname() {
-        return surname;
     }
 
     public int getScore() {
         return score;
     }
 
+    public String getSurname() {
+        return surname;
+    }
+
+    public Student(String surname, int score) {
+        this.score = score;
+        this.surname = surname;
+    }
+
     @Override
-    public String toString() {
-        return "Student{" +
-                "surname='" + surname + '\'' +
-                ", score=" + score +
-                '}';
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return score == student.score &&
+                surname.equals(student.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(score, surname);
     }
 }
