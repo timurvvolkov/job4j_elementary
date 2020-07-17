5b40d4209539cfca9964417b1bee5722bd9cf3aa
timurvvolkov <timurvvolkov@gmail.com>
2020-07-17 16:37:47 +0300

    2. Добавление Maven в существующий проект. [#323770]

---
+++ src/test/java/ru/job4j/strategy/PaintTest.java
@@ -0,0 +1,61 @@
+package ru.job4j.strategy;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.StringJoiner;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PaintTest {
+//    // поле содержит дефолтный вывод в консоль.
+//    private final PrintStream stdout = System.out;
+//    // буфер для результата.
+//    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+//
+//    @Before
+//    public void loadOutput() {
+//        System.out.println("execute before method");
+//        System.setOut(new PrintStream(this.out));
+//    }
+//
+//    @After
+//    public void backOutput() {
+//        System.setOut(this.stdout);
+//        System.out.println("execute after method");
+//    }
+//
+//    @Test
+//    public void whenDrawSquare() {
+//        new Paint().draw(new Square());
+//        assertThat(
+//                this.out.toString(),
+//                is(
+//                        new StringJoiner(System.lineSeparator())
+//                                .add("++++")
+//                                .add("+  +")
+//                                .add("+  +")
+//                                .add("++++")
+//                                .toString()
+//                )
+//        );
+//    }
+//
+//    @Test
+//    public void whenDrawTriangle() {
+//        new Paint().draw(new Triangle());
+//        assertThat(
+//                this.out.toString(),
+//                is(
+//                        new StringJoiner(System.lineSeparator())
+//                                .add("+")
+//                                .add("++")
+//                                .add("+ +")
+//                                .add("++++")
+//                                .toString()
+//                )
+//        );
+//    }
+}
